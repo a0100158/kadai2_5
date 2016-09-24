e3f8917021579d4e05a917576100fd20550b8ffc
SHOUEI\翔栄 <dxbjr492@yahoo.co.jp>
2016-09-24 14:10:57 +0900

    15の倍数で犬になるようにした。

--- Kadai2_5_2.java
+++ Kadai2_5_2.java
@@ -7,14 +7,14 @@ public class Kadai2_5_2 {
 		int intSeisu = scan.nextInt();
 
 		for(int i = 1; i <= intSeisu; i++ ){
-			if(i % 3 == 0 || Integer.toString(i).contains("3") ){
-	            System.out.println(" アホ ");
-			}else if(i % 5 == 0){
-				System.out.println(" ワン ");
-			}else{
-				System.out.println(" " + i + " ");
-			}
-
+		if(i % 5 == 0){
+			System.out.println(" ワン ");
+		}else if(i % 3 == 0 || Integer.toString(i).contains("3")){
+			System.out.println(" アホ ");
+		}else{
+			System.out.println(" " + i + " ");
 		}
+	}
+
 	}
 }
